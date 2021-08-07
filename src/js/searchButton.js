cebc54af02c3cbff5fde01b5392a0c765db8282e
sepaev <sepaev@gmail.com>
2021-08-07 16:27:23 +0300

    svg

--- src/js/searchButton.js
+++ src/js/searchButton.js
@@ -3,5 +3,14 @@ import {getContent} from "./getContent"
 export const onSearchButtonClick = e => {
     console.log('Кликаем поиск');
     console.log(e);
-    getContent(e);
+       Notiflix.Loading.circle();
+    getContent(e)
+        .then(pictures => {
+           Notiflix.Loading.remove();
+          return buildHtml(pictures, page, tag);
+        })
+          .catch(error => {
+           Notiflix.Loading.remove();
+           console.log(error);
+       });
 }
