1436a0d2e1f54087fe3cec42da15b2648e8c4421
sepaev <sepaev@gmail.com>
2021-08-10 02:29:16 +0300

    test json for devs

--- src/js/libraryButtons.js
+++ src/js/libraryButtons.js
@@ -1,8 +1,16 @@
+import { moviesForWatched, moviesForQueue } from './testObjForLocalStor';
+
 export const onWatchedButtonClick = e => {
     console.log('on Watched Button Click');
     console.log(e);
+    const string = JSON.stringify(moviesForWatched)
+    localStorage.setItem('watched', string)
+    alert('B localStorage с ключем "watched" добавлена строка '+string)
 }
 export const onQueueButtonClick = e => {
     console.log('on Queue Button Click');
     console.log(e);
+    const string = JSON.stringify(moviesForQueue)
+    localStorage.setItem('queue', string)
+    alert('B localStorage с ключем "queue" добавлена строка '+string)
 }
--- src/js/searchButton.js
+++ src/js/searchButton.js
@@ -6,12 +6,29 @@ export const onSearchButtonClick = e => {
     console.log(e);
     Notiflix.Loading.pulse();
     getContent(e)
-        .then(film => {
+        .then(data => {
             Notiflix.Loading.remove();
-            return film;
+            return data.results;
         })
+        .then(films => {
+            return films.map(film => {
+                return {
+                    backdrop_path: 'https://image.tmdb.org/t/p/original'+film['backdrop_path'],
+                    id: film['id'],
+                    original_title: film['original_title'],
+                    overview: film['overview'],
+                    popularity: film['popularity'],
+                    poster_path: 'https://image.tmdb.org/t/p/original'+film['poster_path'],
+                    release_date: film['release_date'],
+                    title: film['title'],
+                    vote_average: film['vote_average'],
+                    vote_count: film['vote_count'],
+                };
+            })
+        }).then( obj => console.log(obj))
           .catch(error => {
            Notiflix.Loading.remove();
            console.log(error);
        });
 }
+// <img class="movie__img" data-id="604" src="https://image.tmdb.org/t/p/original//8xEVAe84zlL9rkfYT6dZXero4KK.jpg" alt="The Matrix Reloaded " onerror="this.onerror=null;this.src='https://upload.wikimedia.org/wikipedia/en/d/d6/Image_coming_soon.png';"></img>
