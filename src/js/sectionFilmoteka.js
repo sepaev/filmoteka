f5383d216eda25f8ea9374e60ed972c6ed0e2c78
sepaev <sepaev@gmail.com>
2021-08-10 14:44:46 +0300

    onFilmClick

--- src/js/sectionFilmoteka.js
+++ src/js/sectionFilmoteka.js
@@ -4,13 +4,16 @@ const refs = getRefs();
 const instance = basicLightbox.create(refs.modalFilm);
 
 export const onFilmClick = (e) => {
-    if (e.target.className !== "filmoteka-nav__pages--link") { // для примера
-
-        instance.show(() => console.log('lightbox modal now visible'));
+    if (e.target.className === "film") { // проверка на клик (нужно тестить. может нужно менять)
+        // 1 тут получить id фильма
+        instance.show(() => {console.log('lightbox modal now visible')});// 2 открывает модалку фильма
+        // 3 после открытия реализовать функционал подгрузки innerHTML в модалку по id
+        // 3.1 сначала методом find искать по id  совпаления в localStorage
+        // 3.2 если не найдено - искать в API через функцию getContentById
         // обработка клика по modal film close button
             refs.modalFilmCloseBtn.addEventListener('click', e => {
             e.preventDefault;
-            onFilmCloseClick(); //from "./js/footerDevelopers"  клик по ссылке разработчиков
+            onFilmCloseClick(); //from "./js/footerDevelopers"  клик кнопке закрытия модалки
         });
     }
 }
