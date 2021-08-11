123234080035ec09530d11615f641a5e8f613d2c
sepaev <sepaev@gmail.com>
2021-08-11 23:46:40 +0300

    работа с инпутом

--- src/js/searchBox.js
+++ src/js/searchBox.js
@@ -1,6 +1,13 @@
+import { showPageHome } from './showPage'
+let prevVal = '';
+
 export const onSearchBoxChange = e => {
-    console.log('Изменения searchBox');
-    console.log(e);
+    if (e.key === 'Enter') {
+        showPageHome(1);
+    }
+    let value = prevVal > e.target.value ? '' : e.target.value;
+    prevVal = value;
+    e.target.value = value;
 }
 export const onSearchBoxFocus = e => {
     console.log('Фокус searchBox');
