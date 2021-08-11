fcaf39993a43ead6667d1b6a7abf4edc9b868481
sepaev <sepaev@gmail.com>
2021-08-11 13:33:59 +0300

    обработка запроса апи

---
+++ src/js/parseApiData.js
@@ -0,0 +1,36 @@
+import { genres } from "./consts";
+
+//обрезаю дату
+const trimYear = (fullDate) => {
+    if (!fullDate) return ''
+    return fullDate.slice(0, 4);
+}
+// перевожу жанры в строку
+export const getGenres = (genresIdsFromAPI) => {
+    if (genresIdsFromAPI.length < 1) return '';
+    let genresString = '';
+    genresIdsFromAPI.forEach(genreId => {
+        const genre = genres.find(genre => genre.id === genreId);
+        genresString += genre.name+', ';
+    });
+    return genresString.slice(0, genresString.length - 2);
+}
+
+export const parseFilmsData = (films) => {
+    return films.map(film => {
+        return {
+            backdrop_path: 'https://image.tmdb.org/t/p/original' + film['backdrop_path'],
+            id: film['id'],
+            original_title: film['original_title'],
+            overview: film['overview'],
+            popularity: film['popularity'],
+            poster_path: 'https://image.tmdb.org/t/p/original' + film['poster_path'],
+            release_date: film['release_date'],
+            title: film['title'],
+            vote_average: film['vote_average'],
+            vote_count: film['vote_count'],
+            genres: getGenres(film['genre_ids']),
+            year: trimYear(film['release_date']),
+        };
+    });
+};
