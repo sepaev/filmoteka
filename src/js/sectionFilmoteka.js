707dfa6c5fc32a09fb6839bfe49bf44c9f4e9891
Oleg Usatyi <olegysik1@ukr.net>
2021-08-11 16:10:47 +0300

    new function for close modal by Esc

--- src/js/sectionFilmoteka.js
+++ src/js/sectionFilmoteka.js
@@ -1,19 +1,20 @@
 import * as basicLightbox from 'basiclightbox';
-import { getRefs } from './refs'
-import { loadEscListner } from './escClose'
+import { getRefs } from './refs';
+import { loadEscListner } from './escClose';
 
 const refs = getRefs();
 const instance = basicLightbox.create(refs.modalFilm);
+
 
 export const onFilmClick = (e) => {
     const targetCard = e.target.parentNode.parentNode;
     const targetCardId = targetCard.dataset.id;
     if (targetCard.className === "film") { // проверка на клик (нужно тестить. может нужно менять)
         // 1 тут получить id фильма
-        instance.show(() => {refs.modalCard.innerHTML = 'id ' + targetCardId});// 2 открывает модалку фильма
-            
+        instance.show(() => {refs.modalCard.innerHTML = 'id ' + targetCardId;});// 2 открывает модалку фильма         
         // обработка клика по esc   
         loadEscListner(instance);
+
         // 3 после открытия реализовать функционал подгрузки innerHTML в модалку по id
         // 3.1 сначала методом find искать по id  совпаления в localStorage
         // 3.2 если не найдено - искать в API через функцию getContentById
