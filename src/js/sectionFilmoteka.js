c0612d8b405a9d3c628d0e6d795caa4188929e9b
al-burlachenko <alexandrburl@gmail.com>
2021-08-13 01:47:00 +0300

    корректирует разметку и стили модалки одного фильма, по клику рендерится одна карточка

--- src/js/sectionFilmoteka.js
+++ src/js/sectionFilmoteka.js
@@ -2,32 +2,56 @@ import * as basicLightbox from 'basiclightbox';
 import { getRefs } from './refs';
 import { loadEscListner } from './escClose';
 
+import { fetchGetMovieById } from './getContent';
+import modalFilmCardTpl from '../partials/templates/modalFilmCard.hbs';
+
+import * as parseApiData from './parseApiData';
+
 const refs = getRefs();
 const instance = basicLightbox.create(refs.modalFilm);
 
+const body = document.querySelector('body');
 
-export const onFilmClick = (e) => {
-    const targetCard = e.target.parentNode.parentNode;
-    console.dir(e.target);
-    const targetCardId = targetCard.dataset.id;
-    if (targetCard.className === "film") { // проверка на клик (нужно тестить. может нужно менять)
-        // 1 тут получить id фильма
-        instance.show(() => {refs.modalCard.innerHTML = 'id ' + targetCardId;});// 2 открывает модалку фильма         
-        // обработка клика по esc   
-        loadEscListner(instance);
+export const onFilmClick = e => {
+  // body.style.overflow = 'hidden';
 
-        // 3 после открытия реализовать функционал подгрузки innerHTML в модалку по id
-        // 3.1 сначала методом find искать по id  совпаления в localStorage
-        // 3.2 если не найдено - искать в API через функцию getContentById
-        // обработка клика по modal film close button
-            refs.modalFilmCloseBtn.addEventListener('click', e => {
-            e.preventDefault;
-            onFilmCloseClick(); //from "./js/footerDevelopers"  клик кнопке закрытия модалки
-        });
-    }
-}
+  const targetCard = e.target.parentNode.parentNode;
+  console.dir(e.target);
+  const targetCardId = targetCard.dataset.id;
+  if (targetCard.className === 'film') {
+    // проверка на клик (нужно тестить. может нужно менять)
+    // 1 тут получить id фильма
+    instance.show(() => {
+      // берет данные по фильмам из временного хранилища и парсит их в переменную
+      const currentArray = JSON.parse(localStorage.getItem('tempQuery'));
+      // находит объект по айди
+      const currentItem = currentArray.find(currentItem => currentItem.id === Number(targetCardId));
+
+      if (currentItem === undefined) {
+        fetchGetMovieById(targetCardId).then(data => console.log(data));
+      }
+
+      refs.modalCard.innerHTML = modalFilmCardTpl(currentItem);
+    });
+
+    // 2 открывает модалку фильма
+    // обработка клика по esc
+    loadEscListner(instance);
+
+    // 3 после открытия реализовать функционал подгрузки innerHTML в модалку по id
+    // 3.1 сначала методом find искать по id  совпаления в localStorage
+    // 3.2 если не найдено - искать в API через функцию getContentById
+    // обработка клика по modal film close button
+
+    refs.modalFilmCloseBtn.addEventListener('click', e => {
+      e.preventDefault;
+      onFilmCloseClick(); //from "./js/footerDevelopers"  клик кнопке закрытия модалки
+    });
+  }
+};
 
 const onFilmCloseClick = () => {
-    instance.close();
-}
+  body.style.overflow = '';
 
+  instance.close();
+};
