5ff83acef5af48e82999ee157a231aa21ff876b7
sepaev <sepaev@gmail.com>
2021-08-17 23:13:42 +0300

    test for main

--- src/js/infinitScroll.js
+++ src/js/infinitScroll.js
@@ -5,12 +5,13 @@ export const getHeightOfDocument = () => {
 }
 
 const isBottom = (height, position) => {
-    return (position + 500 > height );
+    return (position + 1000 > height );
 }
 
 export const scrollingWindow = async (currentPage, genreId) => {
     const currentPosition = window.scrollY;
     const bottomPosition = getHeightOfDocument();
+    console.log(currentPosition + 1000 +' > '+ bottomPosition + ' = ' +isBottom(bottomPosition, currentPosition));
     if (isBottom(bottomPosition, currentPosition) && bottomPosition > 1000) {
         return showMorePageHomeGenres(currentPage, genreId)
     };
