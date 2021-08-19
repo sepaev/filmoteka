ec62dc2c0f2d56e8096c6e4130a34123a64dc3f3
sepaev <sepaev@gmail.com>
2021-08-19 12:07:21 +0300

    serch by enter

--- src/js/searchBox.js
+++ src/js/searchBox.js
@@ -1,3 +1,12 @@
+import { showPageHome } from './showPage'
+
 export const onSearchBoxChange = e => {
-    let a;
+    if (e.key === 'Backspace') e.target.value = '';
+    if (e.key === 'Enter') {
+        showPageHome(1);
+    }
+ }
+export const onSearchBoxFocus = e => {
+    // console.log('Фокус searchBox');
+    // console.log(e);
 }
